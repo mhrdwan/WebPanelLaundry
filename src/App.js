1644ffb38bc26c0e3b3d864e2ed01d5049791ce5
mhrdwan <futuremhr@gmail.com>
2023-07-09 18:15:23 +0700

    dua

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Login from './Component/Login';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from './Page/Dashboard';
+import PrivateRoute from './Page/PrivateRoute';
 
 function App() {
   return (
-  <>
-  <Login/>
-  </>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
--- src/Component/Login.js
+++ src/Component/Login.js
@@ -1,16 +1,20 @@
-import { Button, Checkbox, Form, Input , message } from 'antd';
+import { Button, Card, Checkbox, Form, Input, message } from 'antd';
 import { Col, Row } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 function Login() {
-
+    const navigate = useNavigate();
     const dataLogin = {
-        username: "purchasing",
-        password: "purch1",
+        username: "admin",
+        password: "admin",
+        role: "admin"
     };
 
-    const onFinish = (values) => {
-        console.log('Success:', values);
-        if (values.username === dataLogin.username && values.password === dataLogin.password) {
+    const onFinish = (isidata) => {
+        console.log('Success:', isidata);
+        if (isidata.username === dataLogin.username && isidata.password === dataLogin.password) {
             message.success('Login Berhasil');
+            localStorage.setItem("role", "admin");
+            navigate("/dashboard");
         } else {
             message.error('Username atau Password Salah');
         }
@@ -24,73 +28,74 @@ function Login() {
         <>
             <Row style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
                 <Col>
-                    <Form
-                        name="basic"
-                        labelCol={{
-                            span: 8,
-                        }}
-                        wrapperCol={{
-                            span: 16,
-                        }}
-                        style={{
-                            maxWidth: 600,
-                        }}
-                        initialValues={{
-                            remember: true,
-                        }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
-                        autoComplete="off"
-                    >
-                        <Form.Item
-                            label="Username"
-                            name="username"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your username!',
-                                },
-                            ]}
-                        >
-                            <Input />
-                        </Form.Item>
-
-                        <Form.Item
-                            label="Password"
-                            name="password"
-                            rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your password!',
-                                },
-                            ]}
-                        >
-                            <Input.Password />
-                        </Form.Item>
-
-                        <Form.Item
-                            name="remember"
-                            valuePropName="checked"
+                    <Card>
+                        <Form
+                            name="basic"
+                            labelCol={{
+                                span: 8,
+                            }}
                             wrapperCol={{
-                                offset: 8,
                                 span: 16,
                             }}
-                        >
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-
-                        <Form.Item
-                            wrapperCol={{
-                                offset: 8,
-                                span: 16,
+                            style={{
+                                maxWidth: 600,
                             }}
+                            initialValues={{
+                                remember: true,
+                            }}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
                         >
-                            <Button type="primary" htmlType="submit">
-                                Submit
-                            </Button>
-                        </Form.Item>
-                    </Form>
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Masukkan Username!',
+                                    },
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
 
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Masukkan Password!',
+                                    },
+                                ]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item
+                                name="remember"
+                                valuePropName="checked"
+                                wrapperCol={{
+                                    offset: 8,
+                                    span: 16,
+                                }}
+                            >
+                                <Checkbox>Remember me</Checkbox>
+                            </Form.Item>
+
+                            <Form.Item
+                                wrapperCol={{
+                                    offset: 8,
+                                    span: 16,
+                                }}
+                            >
+                                <Button type="primary" htmlType="submit">
+                                    Submit
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </Card>
                 </Col>
             </Row>
 
