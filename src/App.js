27487bcd5d3cf7c5bae1a8081c9b118ef33ab9e8
mhrdwan <futuremhr@gmail.com>
2023-07-09 18:33:05 +0700

    bener

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import Login from './Component/Login';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 import Dashboard from './Page/Dashboard';
 import PrivateRoute from './Page/PrivateRoute';
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="*" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Router>
     </>
   );
 }
